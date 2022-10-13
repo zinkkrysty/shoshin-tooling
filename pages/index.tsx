61fcead9f34adb29e7a40680909b681ed0848228
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-10-13 13:21:30 +0800

    first pass enabling multiple mechs; will move this dev to a new branch

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,11 +17,11 @@ export default function Home() {
     // Constants
     const N_CYCLES = 100
     const ANIM_FRAME_LATENCY = 300
-    const INIT_PROGRAM = 'Z,S,D,X,W,A'
+    const INIT_PROGRAM = 'Z,S,S,D,D,X,W,W,A,A'
+    const DIM = 3
     const MECH_INIT_X = 0
     const MECH_INIT_Y = 0
     const ATOM_INIT_XY = [] // [{x:5, y:3}]
-    const DIM = 2
     const UNIT_STATE_INIT = {
         bg_status: BgStatus.EMPTY,
         border_status: BorderStatus.EMPTY
@@ -30,31 +30,62 @@ export default function Home() {
     for (var i=0; i<DIM; i++){
         unitStatesInit.push(Array(DIM).fill(UNIT_STATE_INIT))
     }
+    const FAUCET_X = 0
+    const FAUCET_Y = 0
+    const SINK_X = DIM-1
+    const SINK_Y = DIM-1
+    const MAX_NUM_MECHS = 3
+    const MIN_NUM_MECHS = 1
 
     // React states
     const [program, setProgram] = useState(INIT_PROGRAM);
     const [instructions, setInstructions] = useState([]);
     const [animationState, setAnimationState] = useState ('Stop');
     const [animationFrame, setAnimationFrame] = useState<number> (0)
-    const [mechInitPos, setMechInitPos] = useState<Grid> ({ x: MECH_INIT_X, y: MECH_INIT_Y })
+    const [mechInitPositions, setMechInitPositions] = useState<Grid[]> ([{ x: MECH_INIT_X, y: MECH_INIT_Y }])
     const [frames, setFrames] = useState<Frame[]>();
     const [loop, setLoop] = useState<NodeJS.Timer>();
+    const [numMechs, setNumMechs] = useState(1)
 
-    let mechInitStates = [
-        { status: MechStatus.OPEN, index: mechInitPos, id: 'mech0', typ: 'singleton' }
-    ] as MechState[]
-    let atomInitStates = ATOM_INIT_XY.map(
+    //
+    // React state updates
+    //
+    const mechInitStates = mechInitPositions.map(
+        (p,_) => { return {status: MechStatus.OPEN, index: p, id: 'mech0', typ: 'singleton'} }
+    ) as MechState[]
+
+    const atomInitStates = ATOM_INIT_XY.map(
         function (xy,i) {
             return {status:AtomStatus.FREE, index:{x:xy.x, y:xy.y}, id:`atom${i}`, typ:'vanilla', possessed_by:null} as AtomState
         }
     )
 
     const frame = frames?.[animationFrame]
-
     const atomStates = frame?.atoms || atomInitStates
     const mechStates = frame?.mechs || mechInitStates
 
-    // Set mech visual
+    let unitStates: UnitState[][]
+    unitStates = setVisualForStates (atomStates, mechStates, unitStatesInit)
+
+    //
+    // Definition of setting DOM state
+    //
+    function setVisualForStates (atomStates: AtomState[], mechStates: MechState[], states: UnitState[][]){
+        let newStates = JSON.parse(JSON.stringify(states)) // duplicate
+
+        for (const atom of atomStates) {
+            newStates = setAtomVisualForStates (atom, newStates)
+        }
+        for (const mech of mechStates) {
+            newStates = setMechVisualForStates (mech, newStates)
+        }
+
+        return newStates
+    }
+
+    //
+    // Definition of setting mech's visual to DOM state
+    //
     function setMechVisualForStates (mech: MechState, states: UnitState[][]){
         let newStates = JSON.parse(JSON.stringify(states)) // duplicate
         if (mech.status == MechStatus.OPEN){
@@ -66,7 +97,9 @@ export default function Home() {
         return newStates
     }
 
-    // Set atom visual
+    //
+    // Definition of setting atom's visual to DOM state
+    //
     function setAtomVisualForStates (atom: AtomState, states: UnitState[][]){
         let newStates = JSON.parse(JSON.stringify(states)) // duplicate
         if (atom.status == AtomStatus.FREE){
@@ -78,7 +111,23 @@ export default function Home() {
         return newStates
     }
 
-    // Handle click event
+    // Handle click event for adding/removing mechs
+    function handleMechClick (mode: string){
+        if (mode === '+' && numMechs < MAX_NUM_MECHS) {
+            setNumMechs (prev => prev+1)
+            setMechInitPositions(
+                Array.from({length:numMechs+1}).fill({ x: MECH_INIT_X, y: MECH_INIT_Y }) as Grid[]
+            )
+        }
+        else if (mode === '-' && numMechs > MIN_NUM_MECHS) {
+            setNumMechs (prev => prev-1)
+            setMechInitPositions(
+                Array.from({length:numMechs-1}).fill({ x: MECH_INIT_X, y: MECH_INIT_Y }) as Grid[]
+            )
+        }
+    }
+
+    // Handle click event for animation control
     function handleClick (mode: string){
         // Run simulation
         if (mode == 'ToggleRun') {
@@ -97,8 +146,8 @@ export default function Home() {
                 // Prepare input
                 const boardConfig = {
                     dimension: DIM as number,
-                    atom_faucets: [{id:'atom_faucet0', typ:'vanilla', index:{x:0,y:0}} as AtomFaucetState],
-                    atom_sinks: [{id:'atom_faucet0', typ:'vanilla', index:{x:DIM-1,y:DIM-1}} as AtomSinkState]
+                    atom_faucets: [{id:'atom_faucet0', typ:'vanilla', index:{x:FAUCET_X,y:FAUCET_Y}} as AtomFaucetState],
+                    atom_sinks: [{id:'atom_faucet0', typ:'vanilla', index:{x:SINK_X,y:SINK_Y}} as AtomSinkState]
                 } as BoardConfig
 
                 // Run simulation to get all frames and set to reference
@@ -142,20 +191,34 @@ export default function Home() {
 
     }
 
-    function setMechInitX (x_str: string){
+    function setMechInitX (mech_i: number, x_str: string){
 
         if (!x_str) return;
         const x = parseInt(x_str)
         if (x < DIM && x >= 0) {
-            setMechInitPos((prev) => ({ ...prev, x }))
+            // setMechInitPos((prev) => ({ ...prev, x }))
+            setMechInitPositions(
+                (prev) => {
+                    let prev_copy = JSON.parse(JSON.stringify(prev))
+                    prev_copy[mech_i] = { ...prev_copy[mech_i], x }
+                    return prev_copy
+                }
+            )
         }
     }
-    function setMechInitY (y_str: string){
+    function setMechInitY (mech_i: number, y_str: string){
 
         if (!y_str) return;
         const y = parseInt(y_str)
         if (y < DIM && y >= 0) {
-            setMechInitPos((prev) => ({ ...prev, y }))
+            // setMechInitPos((prev) => ({ ...prev, y }))
+            setMechInitPositions(
+                (prev) => {
+                    let prev_copy = JSON.parse(JSON.stringify(prev))
+                    prev_copy[mech_i] = { ...prev_copy[mech_i], y }
+                    return prev_copy
+                }
+            )
         }
     }
 
@@ -170,22 +233,11 @@ export default function Home() {
         })
     }
 
-    function setVisualForStates (atomStates: AtomState[], mechStates: MechState[], states: UnitState[][]){
-        let newStates = JSON.parse(JSON.stringify(states)) // duplicate
-
-        for (const atom of atomStates) {
-            newStates = setAtomVisualForStates (atom, newStates)
-        }
-        for (const mech of mechStates) {
-            newStates = setMechVisualForStates (mech, newStates)
-        }
-
-        return newStates
+    function computeUnitTyp (x:number, y:number){
+        if (x == FAUCET_X && y == FAUCET_Y) return 'faucet'
+        else if (x == SINK_X && y == SINK_Y) return 'sink'
+        else return 'regular'
     }
-
-    let unitStates: UnitState[][]
-
-    unitStates = setVisualForStates (atomStates, mechStates, unitStatesInit)
 
     // Render
     return (
@@ -201,39 +253,47 @@ export default function Home() {
                     MovyMovy
                 </h2>
 
-                <div style={{display:'flex', flexDirection:'row', height:'20px'}}>
+                <div style={{display:'flex', flexDirection:'row', height:'20px', marginBottom:'10px'}}>
+                    <button style={{fontSize:'0.75rem', marginRight:'3px'}} onClick={() => handleMechClick('+')}> {'+'} </button>
+                    <button style={{fontSize:'0.75rem', marginRight:'3px'}} onClick={() => handleMechClick('-')}> {'-'} </button>
                     <button style={{fontSize:'0.75rem', marginRight:'3px'}} onClick={() => handleClick('ToggleRun')}> {animationState != 'Run' ? 'Run' : 'Pause'} </button>
                     <button style={{fontSize:'0.75rem', marginRight:'3px'}} onClick={() => handleClick('Stop')}> {'Stop'} </button>
-                    <p style={{padding:'0', textAlign:'center', verticalAlign:'middle', margin:'0', height:'20px', lineHeight:'20px', fontSize:'0.75rem'}}>Frame #{animationFrame}</p>
+                    <p style={{padding:'0', textAlign:'center', verticalAlign:'middle', margin:'0', height:'20px', lineHeight:'20px', fontSize:'0.75rem'}}>Frame# {animationFrame}</p>
                 </div>
 
+                <div className={styles.inputs}>
 
-                <p className={styles.description}>
+                    {
+                        Array.from({length:numMechs}).map ((_,mech_i) => (
+                            <div key={`input-row-${mech_i}`} className={styles.input_row}>
+                                <p style={{margin:'0 10px 0 0', verticalAlign:'middle', height:'20px', lineHeight:'20px'}}>{`mech${mech_i}`}</p>
+                                <input
+                                    className={styles.program}
+                                    onChange={event => {setMechInitX(mech_i, event.target.value)}}
+                                    defaultValue={MECH_INIT_X}
+                                    style={{width:'30px', textAlign:'center'}}
+                                    id={'input-mech-init-x'}
+                                ></input>
 
-                    <input
-                        className={styles.program}
-                        onChange={event => {setMechInitX(event.target.value)}}
-                        defaultValue={MECH_INIT_X}
-                        style={{width:'30px', textAlign:'center'}}
-                        id={'input-mech-init-x'}
-                    ></input>
+                                <input
+                                    className={styles.program}
+                                    onChange={event => {setMechInitY(mech_i, event.target.value)}}
+                                    defaultValue={MECH_INIT_Y}
+                                    style={{width:'30px', textAlign:'center'}}
+                                    id={'input-mech-init-y'}
+                                ></input>
 
-                    <input
-                        className={styles.program}
-                        onChange={event => {setMechInitY(event.target.value)}}
-                        defaultValue={MECH_INIT_Y}
-                        style={{width:'30px', textAlign:'center'}}
-                        id={'input-mech-init-y'}
-                    ></input>
+                                <input
+                                    className={styles.program}
+                                    onChange={event => {setProgram(event.target.value)}}
+                                    defaultValue={INIT_PROGRAM}
+                                    style={{width:'300px'}}
+                                ></input>
+                            </div>
+                        ))
+                    }
 
-                    <input
-                        className={styles.program}
-                        onChange={event => {setProgram(event.target.value)}}
-                        defaultValue={INIT_PROGRAM}
-                        style={{width:'300px'}}
-                    ></input>
-
-                </p>
+                </div>
 
                 <div className={styles.grid_parent}>
                     {
@@ -241,10 +301,11 @@ export default function Home() {
                             <div key={`row-${i}`} className={styles.grid_row}>
                                 {
                                     Array.from({length:DIM}).map ((_,j) => ( // j is x
-                                        // <div id={`cell-${j}-${i}`} key={`cell-${j}-${i}`} className={styles.card}>
-                                        //     ·
-                                        // </div>
-                                        <Unit key={`unit-${j}-${i}`} state={unitStates[j][i]}/>
+                                        <Unit
+                                            key={`unit-${j}-${i}`}
+                                            state={unitStates[j][i]}
+                                            typ={computeUnitTyp(j,i)}
+                                        />
                                     ))
                                 }
                             </div>
--- pages/unit.tsx
+++ pages/unit.tsx
@@ -1,44 +1,58 @@
 import {useState, useEffect, useRef, CSSProperties} from 'react';
 import UnitState, {BgStatus, BorderStatus} from '../src/types/UnitState';
 
-export default function Unit({ state }: { state: UnitState }) {
+export default function Unit({state, typ}) {
 
     // Constants
     const gridStyle = {
         display: 'flex',
-        fontSize: '0.2em',
+        fontSize: '0.6em',
         margin: '0.2rem',
         padding: '0rem',
-        width: '1rem',
-        height: '1rem',
+        width: '1.2rem',
+        height: '1.2rem',
         textAlign: 'center',
         justifyContent: 'center',
         alignItems: 'center',
         color: 'inherit',
         textDecoration: 'none',
-        borderRadius: '0.5rem',
-        transition: 'color 0.15s ease, border-color 0.15s ease',
+        borderRadius: '0.6rem',
+        transition: 'color 0.05s ease, border-color 0.05s ease',
         maxWidth: '300px'
     } as CSSProperties;
 
     // Compute style based on props (from parent's React state)
     var style = gridStyle;
     if (state.bg_status === BgStatus.ATOM_VANILLA_FREE) {
-        style = {...style, backgroundColor: '#DA2C43'}
+        style = {...style, backgroundColor: '#FF355E'}
     }
     else if (state.bg_status === BgStatus.ATOM_VANILLA_POSSESSED) {
-        style = {...style, backgroundColor: '#FF6347'}
+        style = {...style, backgroundColor: '#FF355E'}
     }
 
     if (state.border_status == BorderStatus.SINGLETON_OPEN) {
-        style = {...style, border: '1px solid #DDDDDD'}
+        style = {...style, border: '1px solid #BBBBBB'}
     }
     else if (state.border_status == BorderStatus.SINGLETON_CLOSE) {
-        style = {...style, border: '1px solid #FF7A00'}
+        style = {...style, border: '1px solid #111111'}
+    }
+    else {
+        style = {...style, border: '1px solid #FFFFFF'}
+    }
+
+    var text: string;
+    if (typ == 'faucet') {
+        text = 'F'
+    }
+    else if (typ == 'sink') {
+        text = 'S'
+    }
+    else {
+        text = '·'
     }
 
     // Render
     return (
-        <div style={style} className={'grid'}> · </div>
+        <div style={style} className={'grid'}>{text}</div>
     )
 }
