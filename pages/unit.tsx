0502c8b5c8ea2566a2b1ba57d0e59bd2ac2bbbb3
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-11-05 23:11:03 +0800

    use emoji as atom; the letters showing on top of emoji background is a bit annoying

--- pages/unit.tsx
+++ pages/unit.tsx
@@ -12,49 +12,49 @@ export default function Unit({ state, handleMouseOver, handleMouseOut }) {
     let nuclei: number = 0;
     if (state.bg_status === BgStatus.ATOM_VANILLA_FREE) {
         // style = { backgroundImage:'../src/images/emoji_peach.pn' }
-        // className += styles.atomVanillaFree + ' ';
+        className += styles.atomVanillaFree + ' ';
         // nuclei = 1;
     }
     else if (state.bg_status === BgStatus.ATOM_VANILLA_POSSESSED) {
         // style = { backgroundImage:'../src/images/emoji_peach.pn' }
-        // className += styles.atomVanillaPossessed + ' ';
+        className += styles.atomVanillaPossessed + ' ';
         // nuclei = 1;
     }
 
     else if (state.bg_status === BgStatus.ATOM_HAZELNUT_FREE) {
         className += styles.atomHazelnutFree + ' ' + styles.twoNuclei + ' ';
-        nuclei = 2;
+        // nuclei = 2;
     }
     else if (state.bg_status === BgStatus.ATOM_HAZELNUT_POSSESSED) {
         className += styles.atomHazelnutPossessed + ' ' + styles.twoNuclei + ' ';
-        nuclei = 2;
+        // nuclei = 2;
     }
 
     else if (state.bg_status === BgStatus.ATOM_CHOCOLATE_FREE) {
         className += styles.atomChocolateFree + ' ' + styles.threeNuclei + ' ';
-        nuclei = 3;
+        // nuclei = 3;
     }
     else if (state.bg_status === BgStatus.ATOM_CHOCOLATE_POSSESSED) {
         className += styles.atomChocolatePossessed + ' ' + styles.threeNuclei + ' ';
-        nuclei = 3;
+        // nuclei = 3;
     }
 
     else if (state.bg_status === BgStatus.ATOM_TRUFFLE_FREE) {
         className += styles.atomTruffleFree + ' ' + styles.fourNuclei + ' ';
-        nuclei = 4;
+        // nuclei = 4;
     }
     else if (state.bg_status === BgStatus.ATOM_TRUFFLE_POSSESSED) {
         className += styles.atomTrufflePossessed + ' ' + styles.fourNuclei + ' ';
-        nuclei = 4;
+        // nuclei = 4;
     }
 
     else if (state.bg_status === BgStatus.ATOM_SAFFRON_FREE) {
         className += styles.atomSaffronFree + ' ';
-        nuclei = 1;
+        // nuclei = 1;
     }
     else if (state.bg_status === BgStatus.ATOM_SAFFRON_POSSESSED) {
         className += styles.atomSaffronPossessed + ' ';
-        nuclei = 1;
+        // nuclei = 1;
     }
 
     //
