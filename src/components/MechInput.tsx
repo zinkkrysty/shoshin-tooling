4b2df35d8e07017e21daa608d47216adad14f937
Cristian A <zink.krysty@gmail.com>
2022-11-10 21:36:24 +0100

    Drag and drop reordering of mech inputs (first pass)

--- src/components/MechInput.tsx
+++ src/components/MechInput.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import Grid from "../types/Grid";
-import { isGridOOB } from '../helpers/gridHelpers';
+import { isGridOOB } from "../helpers/gridHelpers";
 import styles from "../../styles/Home.module.css";
 import { useTranslation } from "react-i18next";
-import { PROGRAM_SIZE_MAX } from '../constants/constants';
+import { PROGRAM_SIZE_MAX } from "../constants/constants";
+import { Draggable } from "react-beautiful-dnd";
 
 interface MechInputProps {
     mechIndex: number;
@@ -12,9 +13,9 @@ interface MechInputProps {
     pc: number;
     onPositionChange: (mechIndex: number, position: Grid) => void;
     onProgramChange: (mechIndex: number, program: string) => void;
-    disabled: boolean
-    handleMouseOver: () => void
-    handleMouseOut: () => void
+    disabled: boolean;
+    handleMouseOver: () => void;
+    handleMouseOut: () => void;
 }
 
 const MechInput = ({
@@ -26,96 +27,111 @@ const MechInput = ({
     onProgramChange,
     disabled,
     handleMouseOver,
-    handleMouseOut
+    handleMouseOut,
 }: MechInputProps) => {
-
     const { t } = useTranslation();
 
     const programLength = program.split(",").length;
     const currentInstructionIndex = pc % programLength;
 
-    const NORMAL_STYLE = { width: "700px" }
-    const INVALID_STYLE = {...NORMAL_STYLE, backgroundColor:'#FFCBCB', color:'#999999'}
+    const NORMAL_STYLE = { width: "700px" };
+    const INVALID_STYLE = {
+        ...NORMAL_STYLE,
+        backgroundColor: "#FFCBCB",
+        color: "#999999",
+    };
     const [programStyle, setProgramStyle] = useState(NORMAL_STYLE);
-    const [divStyle, setDivStyle] = useState({})
 
     return (
-        <div key={`input-row-${mechIndex}`} className={styles.input_row}
-            onMouseOver={() => {
-                console.log('yo')
-                setDivStyle(prev => { return {backgroundColor:'#FFFE71', borderRadius:'10px 0 0 10px'} })
-                handleMouseOver()
-            }}
-            onMouseOut={() => {
-                setDivStyle(prev => { return {borderRadius:'10px 0 0 10px'} })
-                handleMouseOut()
-            }}
-            style={divStyle}
-        >
-            <p
-                style={{
-                    margin: "0 1rem 0 3rem",
-                    verticalAlign: "middle",
-                    height: "20px",
-                    lineHeight: "20px",
-                    width: '2.5rem',
-                }}
-            >{t("mech")}{mechIndex}</p>
-            <input
-                className={styles.program}
-                onChange={(event) => {
-                    if (isNaN(parseInt(event.target.value))) return;
-                    onPositionChange(mechIndex, {
-                        ...position,
-                        x: parseInt(event.target.value),
-                    });
-                }}
-                defaultValue={position.x}
-                value={position.x}
-                style={{ width: "30px", textAlign: "center"}}
-                disabled = {disabled}
-            ></input>
-
-            <input
-                className={styles.program}
-                onChange={(event) => {
-                    if (isNaN(parseInt(event.target.value))) return;
-                    onPositionChange(mechIndex, {
-                        ...position,
-                        y: parseInt(event.target.value),
-                    });
-                }}
-                defaultValue={position.y}
-                value={position.y}
-                style={{ width: "30px", textAlign: "center", marginRight: '0.8rem' }}
-                disabled = {disabled}
-            ></input>
-
-            <div className={styles.programWrapper}>
+        <Draggable draggableId={mechIndex.toString()} index={mechIndex}>
+            {(provided, snapshot) => (
                 <div
-                    className={styles.instructionBox}
-                    style={{ left: `${currentInstructionIndex}rem` }}
-                />
-                <input
-                    className={styles.program}
-                    onChange={(event) => {
-                        const program = event.target.value
-                        onProgramChange(mechIndex, program);
-                        const instructions = program.split(',') as string[]
-                        if (instructions.length > PROGRAM_SIZE_MAX) {
-                            setProgramStyle(prev => INVALID_STYLE)
-                        }
-                        else {
-                            setProgramStyle(prev => NORMAL_STYLE)
-                        }
+                    ref={provided.innerRef}
+                    key={`input-row-${mechIndex}`}
+                    className={styles.input_row}
+                    onMouseOver={() => {
+                        handleMouseOver();
                     }}
-                    defaultValue={program}
-                    value={program}
-                    style={programStyle}
-                    disabled = {disabled}
-                ></input>
-            </div>
-        </div>
+                    onMouseOut={() => {
+                        handleMouseOut();
+                    }}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                >
+                    <p
+                        style={{
+                            margin: "0 1rem 0 3rem",
+                            verticalAlign: "middle",
+                            height: "20px",
+                            lineHeight: "20px",
+                            width: "2.5rem",
+                        }}
+                    >
+                        {t("mech")}
+                        {mechIndex}
+                    </p>
+                    <input
+                        className={styles.program}
+                        onChange={(event) => {
+                            if (isNaN(parseInt(event.target.value))) return;
+                            onPositionChange(mechIndex, {
+                                ...position,
+                                x: parseInt(event.target.value),
+                            });
+                        }}
+                        defaultValue={position.x}
+                        value={position.x}
+                        style={{ width: "30px", textAlign: "center" }}
+                        disabled={disabled}
+                    ></input>
+
+                    <input
+                        className={styles.program}
+                        onChange={(event) => {
+                            if (isNaN(parseInt(event.target.value))) return;
+                            onPositionChange(mechIndex, {
+                                ...position,
+                                y: parseInt(event.target.value),
+                            });
+                        }}
+                        defaultValue={position.y}
+                        value={position.y}
+                        style={{
+                            width: "30px",
+                            textAlign: "center",
+                            marginRight: "0.8rem",
+                        }}
+                        disabled={disabled}
+                    ></input>
+
+                    <div className={styles.programWrapper}>
+                        <div
+                            className={styles.instructionBox}
+                            style={{ left: `${currentInstructionIndex}rem` }}
+                        />
+                        <input
+                            className={styles.program}
+                            onChange={(event) => {
+                                const program = event.target.value;
+                                onProgramChange(mechIndex, program);
+                                const instructions = program.split(
+                                    ","
+                                ) as string[];
+                                if (instructions.length > PROGRAM_SIZE_MAX) {
+                                    setProgramStyle((prev) => INVALID_STYLE);
+                                } else {
+                                    setProgramStyle((prev) => NORMAL_STYLE);
+                                }
+                            }}
+                            defaultValue={program}
+                            value={program}
+                            style={programStyle}
+                            disabled={disabled}
+                        ></input>
+                    </div>
+                </div>
+            )}
+        </Draggable>
     );
 };
 
